7b04714fae5191b398373265fc305694d94d3797
Ángela Marín <angelammejia1998@gmail.com>
2022-02-16 17:51:13 -0500

    Encapsulando código en funciones

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## TALLER 1: Figuras Geométricas
 
-- Primer paso: definir las fórmuñas
+- Primer paso: definir las fórmulas
 - Segundo paso: implementar las fórmulas en JS
 - Tercer paso: crear funciones
 - Cuarto paso: integrar JS con HTML
--- figuras.js
+++ figuras.js
@@ -1,53 +1,33 @@
 // Square code
-console.group("Square");
+function squarePerimeter(side) {
+   return side * 4;
+}
 
-const squareSide = 5;
-console.log("Each side of the square is equal to: " + squareSide + " cm");
-
-const squarePerimeter = squareSide * 4;
-console.log("The perimeter of the square is equal to: " + squarePerimeter + " cm");
-
-const squareArea = squareSide * squareSide;
-console.log("The area of the square is equal to: " + squareArea + " cm^2");
-
-console.groupEnd();
+function squareArea(side) {
+    return side * side;
+} 
 
 // Triangle code
-console.group("Triangle");
+function trianglePerimeter(side1, side2, base) {
+    return side1 + side2 + base;
+}
 
-const triangleSide1 = 6;
-const triangleSide2 = 6;
-const triangleBase = 4;
-console.log("The sides of the triangle are equal to: " + triangleSide1 + " cm, " + triangleSide2 + " cm and " + triangleBase + " cm");
+function triangleArea(base, height) {
+    return (base * height) / 2;
+}
 
-const trianglePerimeter = triangleSide1 + triangleSide2 + triangleBase;
-console.log("The perimeter of the triangle is equal to: " + trianglePerimeter + " cm");
-
-
-const triangleHeight = 5.5;
-console.log("The height of the triangle is equal to: " + triangleHeight + " cm");
-
-
-const triangleArea = (triangleBase * triangleHeight) / 2;
-console.log("The area of the triangle is equal to: " + triangleArea + " cm^2");
-
-console.groupEnd();
-
-// Triangle code
-console.group("Circle");
-
-const circleRadio = 4;
-console.log("The radio of the circle is equal to: " + circleRadio);
-
-const circleDiameter = circleRadio * 2;
-console.log("The diameter of the circle is equal to: " + circleDiameter);
-
+// Circle code
 const PI = Math.PI;
 
-const circlePerimeter = circleDiameter * PI;
-console.log("The perimeter of the circle is equal to: " + circlePerimeter + " cm");
+function circleDiameter(radio) { 
+    return radio * 2;
+}
 
-const circleArea = (circleRadio * circleRadio) * PI;
-console.log("The area of the circle is equal to: " + circleArea + " cm^2");
+function circlePerimeter(radio) {
+    const diameter = circleDiameter(radio); 
+    return diameter * PI;
+}
 
-console.groupEnd();
+function circleArea(radio) {
+    (radio * radio) * PI;
+}
