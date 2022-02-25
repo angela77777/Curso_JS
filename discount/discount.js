ee177a1008c81ce9ce44b2a671ae6d2f6f4bfc95
Ángela Marín <angelammejia1998@gmail.com>
2022-02-25 16:26:46 -0500

    Iniciando el segundo taller

---
+++ discount/discount.js
@@ -0,0 +1,19 @@
+//const originalPrice = 120;
+//const discount = 18;
+
+function calculateDiscountedPrice(price, discount) {
+    const discountedPricePercentage = 100 - discount;
+    const discountedPrice = (price * discountedPricePercentage) / 100;
+
+    return discountedPrice;
+}
+
+
+
+/*console.log({
+    originalPrice,
+    discount,
+    discountedPricePercentage,
+    discountedPrice
+});*/
+
